ba502241759ce67dffd862e6f475f59321ea2c19
dtscott9 <dtscott9@byui.edu>
2022-02-08 09:58:12 -0700

    working on jump

--- unit03-jumper/Jumper.cs
+++ unit03-jumper/Jumper.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 namespace jumper 
 {
 
     public class Jumper 
     {
+        private List<string> man = new List<string>();
+
         
         public Jumper()
         {
--- unit03-jumper/guess.cs
+++ unit03-jumper/guess.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace Game.Guess
 {
     public class Guess
     {
-        public List<int> _guessProgress;
+        public List<int> _guessProgress = new List<int>();
+        
 // cat character a
         public void CheckGuess(char userGuess, string hangManWord)
         {
