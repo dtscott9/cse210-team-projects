8a45c55b366179fee3c5fed12bdd20d9394b8990
jdsmith2004 <97774477+jdsmith2004@users.noreply.github.com>
2022-02-10 10:24:53 -0700

    d

--- unit03-jumper/terminal.cs
+++ unit03-jumper/terminal.cs
@@ -7,23 +7,24 @@ namespace terminal
         private string displayWord;
         private string _playerGuess;
 
-        public void SetPlayerGuess(string playerGuess)
+        public void SetPlayerGuess()
         {
             Console.WriteLine("Guess a chararacter");
-            playerGuess = Console.ReadLine();
-            if (playerGuess.Length == 1)
-            {
-                _playerGuess = playerGuess;
-            } else
-            {
-                Console.WriteLine("Sorry, your guess is too long");
-            }
+            char playerGuess = Console.ReadLine();
+            // if (playerGuess.Length == 1)
+            // {
+            //     _playerGuess = playerGuess;
+            // } else
+            // {
+            //     Console.WriteLine("Sorry, your guess is too long");
+            // }
         }
-        
+
         public string GetPlayerGuess()
         {
             return _playerGuess;
         }
+        //TODO input guess
         public string CreateDisplayWord(string guessWord) //gues word is the word the user trys to guess
         {
             string tempWord = "";
