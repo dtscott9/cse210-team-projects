18939ba30c4b40a88d562cd64f2b9f204f9f7a3e
Jeff Smith <swampfirejeffrey@gmail.com>
2022-03-26 13:02:36 -0600

    added foreach for enemy

--- Experiment/Game/Scripting/ProjectileFireAction.cs
+++ Experiment/Game/Scripting/ProjectileFireAction.cs
@@ -13,27 +13,34 @@ namespace Unit06.Game.Scripting
 
         public async void Execute(Cast cast, Script script, ActionCallback callback)
         {
-            foreach(Turret turret in cast.GetActors(Constants.TURRET_GROUP))
+            foreach (Turret turret in cast.GetActors(Constants.TURRET_GROUP))
             {
                 Body Tbody = turret.GetBody();
-                if(turret.ShouldFire())
+                if (turret.ShouldFire())
                 {
-                    Console.WriteLine("success");
-                
-                    Point position = Tbody.GetPosition();
-                    Point size = new Point(Constants.PROJECTILE_WIDTH, Constants.PROJECTILE_HEIGHT);
-                    Point velocity = new Point(0, 10);
 
-                    Body body = new Body(position, size, velocity);
-                    Image image = new Image(Constants.PROJECTILE_IMAGE);
-                    Projectile projectile = new Projectile(body, image, false);
-                    cast.AddActor(Constants.PROJECTILE_GROUP, projectile);
-                    turret.ResetCountdown();
+                    foreach (Enemy enemy in cast.GetActors(Constants.ENEMY_GROUP))
+                    {
+                        Body eBody = enemy.GetBody();
+                        Point ePosition = eBody.GetPosition();
+                    }
+                        Console.WriteLine("success");
+
+                        Point position = Tbody.GetPosition();
+                        Point size = new Point(Constants.PROJECTILE_WIDTH, Constants.PROJECTILE_HEIGHT);
+                        Point velocity = new Point(0, 10);
+
+                        Body body = new Body(position, size, velocity);
+                        Image image = new Image(Constants.PROJECTILE_IMAGE);
+                        Projectile projectile = new Projectile(body, image, false);
+                        cast.AddActor(Constants.PROJECTILE_GROUP, projectile);
+                        turret.ResetCountdown();
+                    
                 }
                 turret.CountDown();
             }
-            
-        }   
+
+        }
 
     }
 
