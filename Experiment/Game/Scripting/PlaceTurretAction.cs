8c8359a8c9defc371c9ce02b1e9b6675d3174365
Jeff Smith <swampfirejeffrey@gmail.com>
2022-03-31 14:53:39 -0600

    Turret Spacing

--- Experiment/Game/Scripting/PlaceTurretAction.cs
+++ Experiment/Game/Scripting/PlaceTurretAction.cs
@@ -5,7 +5,7 @@ using Unit06.Game.Services;
 namespace Unit06.Game.Scripting
 {
     public class PlaceTurret : Action
-    {   
+    {
         private AudioService audioService;
         private MouseService mouseService;
 
@@ -20,41 +20,55 @@ namespace Unit06.Game.Scripting
             Point mouseCor = mouseService.GetCoordinates();
             int mouseX = mouseCor.GetX();
             int mouseY = mouseCor.GetY();
-            Random random1 = new Random();
-            Stats stats = (Stats)cast.GetFirstActor(Constants.STATS_GROUP);
-            int AvailableGold = stats.GetGoldCount();
-            if (mouseService.IsButtonReleased(Constants.LEFT))
+            bool placeTurretBool = true;
+
+            foreach (Turret turret in cast.GetActors(Constants.TURRET_GROUP))
             {
-                if (mouseY >= Constants.TURRET_PLACEMENT_Y_1 && mouseY <= Constants.TURRET_PLACEMENT_Y_1 + 45
-                || mouseY <= Constants.TURRET_PLACEMENT_Y_2 && mouseY >= Constants.TURRET_PLACEMENT_Y_2 - 45
-                || mouseY >= Constants.TURRET_PLACEMENT_Y_2 && mouseY <= Constants.TURRET_PLACEMENT_Y_2 + 45)
+                Body body = turret.GetBody();
+                Point turretPosition = body.GetPosition();
+                int turretX = turretPosition.GetX();
+                if (Math.Abs(turretX - mouseX) <= 30)
                 {
-                    if (AvailableGold >= 100)
+                    placeTurretBool = false;
+                }
+            }
+            if (placeTurretBool)
+            {
+                Stats stats = (Stats)cast.GetFirstActor(Constants.STATS_GROUP);
+                int AvailableGold = stats.GetGoldCount();
+                if (mouseService.IsButtonReleased(Constants.LEFT))
+                {
+                    if (mouseY >= Constants.TURRET_PLACEMENT_Y_1 && mouseY <= Constants.TURRET_PLACEMENT_Y_1 + 45
+                    || mouseY <= Constants.TURRET_PLACEMENT_Y_2 && mouseY >= Constants.TURRET_PLACEMENT_Y_2 - 45
+                    || mouseY >= Constants.TURRET_PLACEMENT_Y_2 && mouseY <= Constants.TURRET_PLACEMENT_Y_2 + 45)
                     {
-                        Image image = Constants.TURRET_IMAGE_DOWN;
-                        if (mouseY <= 340)
+                        if (AvailableGold >= 100)
                         {
-                            mouseY = Constants.TURRET_PLACEMENT_Y_1;
-                            image = Constants.TURRET_IMAGE_DOWN;
-                        }
-                        else
-                        {
-                            mouseY = Constants.TURRET_PLACEMENT_Y_2;
-                            image = Constants.TURRET_IMAGE_UP;
-                        }
-                        int cost = 100;
-                        Point position = new Point(mouseX, mouseY);
-                        Point size = new Point(Constants.TURRET_WIDTH, Constants.TURRET_HEIGHT);
-                        Point velocity = new Point(0, 0);
+                            Image image = Constants.TURRET_IMAGE_DOWN;
+                            if (mouseY <= 340)
+                            {
+                                mouseY = Constants.TURRET_PLACEMENT_Y_1;
+                                image = Constants.TURRET_IMAGE_DOWN;
+                            }
+                            else
+                            {
+                                mouseY = Constants.TURRET_PLACEMENT_Y_2;
+                                image = Constants.TURRET_IMAGE_UP;
+                            }
+                            int cost = 100;
+                            Point position = new Point(mouseX, mouseY);
+                            Point size = new Point(Constants.TURRET_WIDTH, Constants.TURRET_HEIGHT);
+                            Point velocity = new Point(0, 0);
 
-                        Body body = new Body(position, size, velocity);
-                        
-                        Turret turret = new Turret(body, image, false);
-                        cast.AddActor(Constants.TURRET_GROUP, turret);
-                        Sound sound = new Sound(Constants.PURCHASE_SOUND);
-                        audioService.PlaySound(sound);
+                            Body body = new Body(position, size, velocity);
 
-                        stats.SubtractGold(cost);
+                            Turret turret = new Turret(body, image, false);
+                            cast.AddActor(Constants.TURRET_GROUP, turret);
+                            Sound sound = new Sound(Constants.PURCHASE_SOUND);
+                            audioService.PlaySound(sound);
+
+                            stats.SubtractGold(cost);
+                        }
                     }
                 }
             }
